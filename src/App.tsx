60f173b0d02a8e90250bcd00e5d466ef25eca650
Polina Simonenko <psimonen@c2s15d84.42wolfsburg.de>
2023-08-08 15:00:12 +0200

    feat: created basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    ActionIcon,
+    Button,
+    NavLink
+}   from '@mantine/core';
+import {
+    ReactComponent as GithubIcon
+}   from "./icons/github.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Polina Simonenko
+                </p>
+                <ActionIcon
+                    size="lg"
+                    variant='outline'
+                    radius='lg'
+                    color="violet"
+                    component="a"
+                    href="https://github.com/rabarbra"
+                    target="_blank"
+                >
+                    <GithubIcon/>
+                </ActionIcon>
+            </header>
+        </div>
+    );
 }
 
 export default App;
