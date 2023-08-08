1c576dbd8bf5d9fb0ac1532df8dbae1f66ea84dd
Polina Simonenko <psimonen@c2s15d84.42wolfsburg.de>
2023-08-08 20:27:54 +0200

    feat: added themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,85 @@
 import React from 'react';
 import './App.css';
 import {
-    ActionIcon, Group
+    ActionIcon,
+    Group,
+    MantineProvider,
+    ColorSchemeProvider,
+    ColorScheme,
+    AppShell,
+    Header
 }   from '@mantine/core';
 import {
+    useColorScheme
+}   from '@mantine/hooks';
+import {
+    IconSun,
+    IconMoonStars
+}   from '@tabler/icons-react';
+import {
     ReactComponent as GithubIcon
-}   from "./icons/github.svg";
+}   from './icons/github.svg';
 import {
     ReactComponent as LinkedinIcon
-}   from "./icons/linkedin.svg";
+}   from './icons/linkedin.svg';
 
 function App() {
+    const preferredColorScheme = useColorScheme("dark");
+    const [colorScheme, setColorScheme] = React.useState<ColorScheme>(preferredColorScheme);
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Polina Simonenko
-                </p>
-                <Group>
-                    <ActionIcon
-                        size="lg"
-                        variant='outline'
-                        radius='lg'
-                        color="violet"
-                        component="a"
-                        href="https://github.com/rabarbra"
-                        target="_blank"
-                    >
-                        <GithubIcon/>
-                    </ActionIcon>
-                    <ActionIcon
-                        size="lg"
-                        variant='outline'
-                        radius='lg'
-                        color="violet"
-                        component="a"
-                        href="https://www.linkedin.com/in/polina-simonenko"
-                        target="_blank"
-                    >
-                        <LinkedinIcon/>
-                    </ActionIcon>
-                </Group>
-            </header>
-        </div>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+                <AppShell
+                    header={
+                        <Header height={30} withBorder={false}>
+                            <ActionIcon
+                                ml='auto'
+                                mr={5}
+                                color='violet'
+                                onClick={() => toggleColorScheme()}
+                                title="Toggle color scheme"
+                            >
+                                {colorScheme === 'dark' ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
+                            </ActionIcon>
+                        </Header>
+                    }
+                >
+                <div className="App">
+                    <header className="App-header">
+                        <p>
+                            Polina Simonenko
+                        </p>
+                        <Group>
+                            <ActionIcon
+                                size="lg"
+                                variant='outline'
+                                radius='lg'
+                                color="violet"
+                                component="a"
+                                href="https://github.com/rabarbra"
+                                target="_blank"
+                            >
+                                <GithubIcon/>
+                            </ActionIcon>
+                            <ActionIcon
+                                size="lg"
+                                variant='outline'
+                                radius='lg'
+                                color="violet"
+                                component="a"
+                                href="https://www.linkedin.com/in/polina-simonenko"
+                                target="_blank"
+                            >
+                                <LinkedinIcon/>
+                            </ActionIcon>
+                        </Group>
+                    </header>
+                </div>
+                </AppShell>
+            </MantineProvider>
+        </ColorSchemeProvider>
     );
 }
 
